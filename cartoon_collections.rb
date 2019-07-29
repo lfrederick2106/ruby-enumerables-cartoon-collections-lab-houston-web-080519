2e4a3ffebe299adda2766e294faff44887ff9c16
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 03:26:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.select { |el| el.any?
+  array.select { |el| el.any? {el == } }
 end
