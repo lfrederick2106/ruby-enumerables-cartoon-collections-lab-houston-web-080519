62a3db8997f0db22e2e50e7c833314913fec7d59
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 03:53:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{|dwarves, i| puts "#{i + 1}. #{dwarves}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! {|name| name.capitalize }
+  planeteer_calls.collect {|x| x + "!"}  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |str| str.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.select { |i| i == "cheddar" || i == "gouda" || i == "camembert"}.first
 end
