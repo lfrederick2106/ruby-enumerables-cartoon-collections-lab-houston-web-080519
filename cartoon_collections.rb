545c512b2c114488e3615455457a3e00e0f2810d
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 03:38:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  array.select {i == "cheddar" || i == "gouda" || i == "camembert"}
+ 
+  array.select { |i| i == "cheddar" || i == "gouda" || i == "camembert"}
 end
