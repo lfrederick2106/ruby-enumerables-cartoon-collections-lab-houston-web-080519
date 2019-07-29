1789f22d5d0ae9a73f02420dad7f05b695c94257
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-29 03:02:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  strings.any? { |str| str.size > 4 }
+  calls.any? { |str| str.size > 4 }
 end
 
 def find_the_cheese# code an argument here
